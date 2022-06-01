ce8bb5ee93f4b1d78fc8298c8664c8ba5214b185
Vinicius Gasparello da Silva <gasparellovinicius@gmail.com>
2022-06-01 20:53:08 -0300

    Primeiro Commit

---
+++ PastasAutomatizadas/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string userName = Environment.UserName;
+
+string source_dir = @$"C:\Users\{userName}\Downloads";
+string destination_dir_image = @$"C:\Users\{userName}\Pictures";
+string destination_dir_documents = @$"C:\Users\{userName}\Documents";
+
+string[] images_extentions = { ".jpg", ".png" };
+string[] documents_extentions = { ".doc", ".pdf", ".docx", ".txt", ".xls", ".xlsx"};
+
+
+Executar();
+
+
+
+void Executar()
+{
+    Console.WriteLine(source_dir);
+
+}
+
+
+void MoveFiles(string destination, string entry, string name)
+{
+
+}
